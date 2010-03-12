05acec403eba6c2cac2dab7f8013afcb5d91912f
HuyT <thailehuy@gmail.com>
2010-03-12 19:32:32 +0700

    change paginate

--- app/controllers/youth_controller.rb
+++ app/controllers/youth_controller.rb
@@ -7,10 +7,10 @@ class YouthController < ApplicationController
     @friends = fbsession.users_getInfo(:uids => friend_uids,
             :fields => ["first_name", "pic_square", "profile_url"]).user_list
 
-    featured_event_eids = Feature.paginate(:all, 
-      :conditions => {:f_type => "event"}, :page => 1, :per_page => PER_PAGE).map{|e| e.eid.to_s}
-    featured_gathering_eids = Feature.paginate(:all, 
-      :conditions => {:f_type => "gathering"}, :page => 1, :per_page => PER_PAGE).map{|e| e.eid.to_s}
+    featured_event_eids = Feature.find(:all,
+      :conditions => {:f_type => "event"}, :limit => PER_PAGE).map{|e| e.eid.to_s}
+    featured_gathering_eids = Feature.find(:all,
+      :conditions => {:f_type => "gathering"}, :limit => PER_PAGE).map{|e| e.eid.to_s}
     unless featured_event_eids.empty?
       @featured_events = fbsession.events_get(:eids => featured_event_eids).event_list
     end
