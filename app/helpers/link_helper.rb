30b33aa7256c70eeacfaeb9a613b669729b22f1c
HuyT <thailehuy@gmail.com>
2010-03-12 19:54:29 +0700

    add menu

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,5 +1,6 @@
 module LinkHelper
-  MENU_ITEMS = [
+  def menu_items
+  [
     ["Home", "/"],
     ["About", url_for(:controller => "youth", :action => "about")],
     ["Festival Guide", url_for(:controller => "youth", :action => "guide")],
@@ -8,13 +9,14 @@ module LinkHelper
     ["Book ticket", url_for(:controller => "youth", :action => "booking")],
     ["RM50,000 Giveaway", url_for(:controller => "youth", :action => "giveaway")]
   ]
+  end
 
   def event_link(event)
     "http://www.facebook.com/event.php?eid=#{event.eid}"
   end
 
   def navigation_menu current_tab = ""
-    links = MENU_ITEMS.map{|m|
+    links = menu_items.map{|m|
       %Q{
         <li class="#{current_tab == m[0] ? "current" : ""}">
           #{link_to(m[0], m[1], :class => "navlink")}
