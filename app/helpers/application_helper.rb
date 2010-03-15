1fb03e335508212b6c358453bfa381e7fb4a8b16
HuyT <thailehuy@gmail.com>
2010-03-15 23:31:56 +0700

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def display_time time_stamp
-    Time.at(time_stamp.to_i).strftime("%a %b %d, %H:%M")
+    Time.at(time_stamp.to_i).strftime("%b %d, %H:%M")
   end
 end
