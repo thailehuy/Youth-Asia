5f8f1390504845a1d4d75b1a0cc74c569ee3c310
fird <ross@youthasia.com>
2010-03-22 18:31:50 +0800

    initial style for home and about

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100312125058) do
+ActiveRecord::Schema.define(:version => 20100321070859) do
+
+  create_table "contents", :force => true do |t|
+    t.text     "volunteer_opp"
+    t.string   "video_link"
+    t.text     "about_youthsay"
+    t.text     "about_youth10"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "db_files", :force => true do |t|
+    t.binary "data"
+  end
 
   create_table "events", :force => true do |t|
     t.string   "link"
     t.string   "eid"
+    t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -39,11 +53,22 @@ ActiveRecord::Schema.define(:version => 20100312125058) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "eid"
   end
 
   create_table "invitation_counts", :force => true do |t|
     t.string   "uid"
-    t.integer  "counter"
+    t.string   "invited_uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tickets", :force => true do |t|
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "ic_number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -58,9 +83,21 @@ ActiveRecord::Schema.define(:version => 20100312125058) do
     t.string   "address"
     t.string   "race"
     t.string   "ic_number"
+    t.string   "position_1"
+    t.string   "position_2"
+    t.string   "position_3"
     t.text     "reason"
+    t.string   "content_type"
+    t.string   "filename"
+    t.string   "thumbnail"
+    t.integer  "size"
+    t.integer  "width"
+    t.integer  "height"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "school",        :default => ""
+    t.string   "state",         :default => ""
+    t.integer  "db_file_id"
   end
 
 end
