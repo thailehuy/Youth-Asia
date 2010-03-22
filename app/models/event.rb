9f4fca0c29248c0944eb8fba72dab084617d2a5f
U-LOGIXE-E5EEAF6C\Administrator <Administrator@logixe-e5eeaf6c.(none)>
2010-03-22 11:55:22 +0700

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,6 @@ class Event < ActiveRecord::Base
     "Conferences"
   ]
 
-  validates_format_of :link, :with => /^http:\/\/(www.)?facebook.com\/event.php\?eid=\d+(&|$)/i
+  validates_format_of :link, :with => /^http:\/\/(www.)?facebook.com\/event.php\?(.*&?)eid=\d+(&|$)/i
   validates_inclusion_of :category, :in => CATEGORIES
 end
