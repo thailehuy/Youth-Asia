b8177613f820e8a6dac71e20a37931719c500746
HuyT <thailehuy@gmail.com>
2010-03-23 23:28:39 +0700

    mailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def contact_submit(name, email, reason)
     recipients "thailehuy@yahoo.com"
-    from       "chip@youthasia.com"
+    from       "dev@youthsays.com"
     subject    "New inquiries"
     sent_on    Time.now
     body       :name => name, :email => email, :reason => reason
